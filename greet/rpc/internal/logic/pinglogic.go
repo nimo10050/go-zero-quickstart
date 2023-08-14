9593766b0d336d22b4724923093bf4d0e18d34d0
DESKTOP-3HHRMHU\zgp <nimo10050@gmail.com>
2023-08-15 00:07:53 +0800

    init commit

---
+++ greet/rpc/internal/logic/pinglogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"quickstart/greet/rpc/internal/svc"
+	"quickstart/greet/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PingLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
+	return &PingLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *PingLogic) Ping(in *pb.Placeholder) (*pb.Placeholder, error) {
+	// todo: add your logic here and delete this line
+
+	return &pb.Placeholder{}, nil
+}
